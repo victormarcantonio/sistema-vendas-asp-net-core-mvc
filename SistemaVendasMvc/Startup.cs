fa2d7d6e39f3a9e6f6ed259379a5fd3ca38418d2
Victor Marcantonio <victor_marcantonio@hotmail.com>
2019-03-21 13:38:50 -0300

    Adaptação para MySql e primeira migração

--- SistemaVendasMvc/Startup.cs
+++ SistemaVendasMvc/Startup.cs
@@ -37,7 +37,8 @@ namespace SistemaVendasMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SistemaVendasMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SistemaVendasMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SistemaVendasMvcContext"), builder =>
+                        builder.MigrationsAssembly("SistemaVendasMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
