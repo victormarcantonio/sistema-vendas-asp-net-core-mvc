30ea4bcec37bf0ed9ff5dba516ec30451c910a64
Victor Marcantonio <victor_marcantonio@hotmail.com>
2019-03-28 00:58:56 -0300

    Tela de detalhes e eager loading

--- SistemaVendasMvc/Controllers/VendedoresController.cs
+++ SistemaVendasMvc/Controllers/VendedoresController.cs
@@ -63,5 +63,21 @@ namespace SistemaVendasMvc.Controllers
             _vendedorService.Remover(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Detalhes(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _vendedorService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
--- SistemaVendasMvc/Services/VendedorService.cs
+++ SistemaVendasMvc/Services/VendedorService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SistemaVendasMvc.Services
 {
@@ -27,7 +28,8 @@ namespace SistemaVendasMvc.Services
         }
         public Vendedor FindById(int id)
         {
-            return _context.Vendedor.FirstOrDefault(obj => obj.Id == id);
+            return _context.Vendedor.Include(obj=> obj.Departamento).FirstOrDefault(obj => obj.Id == id);
+
         }
         public void Remover(int id)
         {
